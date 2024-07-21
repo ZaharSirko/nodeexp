ae4e3c94283430e35cb7433148e23ed66bb3c651
ZaharSirko <magezku@gmail.com>
2024-07-21 15:35:21 +0300

    commit

--- server.js
+++ server.js
@@ -1,5 +1,20 @@
 const express = require('express');
 const app = express();
+const mysql = require('mysql2');
+const port = 3000;
+const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+});
+
+db.connect(err => {
+    if (err) {
+        console.log(err);
+    }
+    console.log('MySQL Connected...');
+});
 
 app.get("/", (req, res) => {
     console.log("here");
@@ -11,4 +26,6 @@ const userRouter = require("./routes/users");
 
 app.use("/users",userRouter);
 
-app.listen(8080);
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
