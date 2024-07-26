ad3f148ac439d1d8bb82639b3dcb5c5aa6b7cb6f
ZaharSirko <magezku@gmail.com>
2024-07-26 17:08:28 +0300

    commit

--- src/data-source.ts
+++ src/data-source.ts
@@ -3,9 +3,3 @@ import { DataSource } from 'typeorm';
 import config from '../ormconfig';
 
 export const AppDataSource = new DataSource(config);
-
-AppDataSource.initialize().then(() => {
-    console.log("Data Source has been initialized!");
-}).catch((err) => {
-    console.error("Error during Data Source initialization:", err);
-});
